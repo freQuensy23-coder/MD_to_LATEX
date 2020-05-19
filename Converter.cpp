221af5ad342c406c23b625d09698365886fd04f6
freQuensy <mametevalex@gmail.com>
2020-05-19 17:12:45 +0300

    Added lines and trying of codeblocks

--- Converter.cpp
+++ Converter.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 bool images_found = false;
 bool links_found = false;
+bool code_area_found = false;
 
+string file_out;
 
 string replace_heading(string markdown) {
 	//There are 6 heading's type in MD, and only 5 in Latex, so ## and ### linked to the same tag "\huge"
@@ -89,9 +91,9 @@ string replace_bold_alternative(string MD) {
 	//__Bold text. Spaces are avalible. "_" too, but only single__
 	//\textbf{Bold text. Spaces are avalible. "_" too, but single}
 
-	regex bold_text("\\*\\*[^\\*{1}](.*?)\\*\\*");
+	regex bold_text("\\*\\*([^\\*{1}])(.*?)\\*\\*");
 
-	MD = regex_replace(MD, bold_text, "\\textbf{$1}");
+	MD = regex_replace(MD, bold_text, "\\textbf{$1$2}");
 
 	return MD;
 }
@@ -113,13 +115,47 @@ string replace_italic_alternative(string MD) {
 }
 
 
+string replace_line(string MD) {
+	regex line("\\*\\*\\*");
+
+	MD = regex_replace(MD, line, "\\noindent\\rule{\\textwidth}{1pt}");
+
+	return MD;
+}
+
+
+string replace_line_alternative(string MD) {
+	regex line("---");
+
+	MD = regex_replace(MD, line, "\\noindent\\rule{\\textwidth}{1pt}");
+
+	return MD;
+}
+
+
+string replace_code_area(string MD) {
+	regex line("```(.*)\n([.\n]*)```");
+	
+	string tex = regex_replace(MD, line, "\\lstset{language=$1}\n\\begin{lstlisting}$2\end{lstlisting}");
+
+	if (tex != MD) {
+		::code_area_found = true;
+	}
+
+	return tex;
+}
+
+
 string module_manager() {
 	string modules = "";
 	if (::images_found) {
-		modules = modules + "\\usepackage{graphicx}" + "\n";
+		modules = modules + "\\usepackage{graphicx}" + "\n";//Pictures module
 	}
 	if (::links_found) {
-		modules = modules + "\\usepackage{hyperref}" + "\n";
+		modules = modules + "\\usepackage{hyperref}" + "\n";//links module
+	}
+	if (::code_area_found) {
+		modules = modules + "\\usepackage{listings}" + "\n";//code area module
 	}
 	return modules;
 }
@@ -135,6 +171,9 @@ string convert(string MD)
 	MD = replace_bold_alternative(MD);
 	MD = replace_italic(MD);
 	MD = replace_italic_alternative(MD);
+	//MD = replace_line(MD);
+	MD = replace_line_alternative(MD);
+	MD = replace_code_area(MD);
 	MD = "\\begin{document} \n" + MD;        //preamble
 	MD = module_manager() + MD;
 	MD = "\\documentclass{article} \n " + MD + "\n\\end{document}";
@@ -163,6 +202,16 @@ string file_read(string file_name)
 }
 
 
+bool get_output_filename(string file_input)
+{
+	regex file_name("\\.[0-9a-z]{2}$");
+
+	::file_out = regex_replace(file_input, file_name, ".tex");
+	if (::file_out == file_input) { return true; }
+	else { return false; }
+}
+
+
 void file_write(string file_name, string text) {
 	try
 	{
@@ -178,22 +227,33 @@ void file_write(string file_name, string text) {
 
 int main(int argc, char* argv[])
 {
+
+
+
 	//Getting file name
 	if (true) {
 		string file_input = argv[1];
-		string file_out = "output.tex";
-		cout << "Opening " << argv[1] << "\n";
-		string MD = file_read(file_input);
 
 
-		//Saving and Converting 
-		if (MD != "") {
-			string Tex = convert(MD);
-			file_write(file_out, Tex);
-			cout << "Saved as " << file_out;
+		if (get_output_filename(file_input))
+		{
+			cout << "Wrong file_name. It should ended to .md";
 		}
-		else {
-			cout << "Error while reading file. It may be empty or not exist at all";
+		else
+		{
+
+			string MD = file_read(file_input);
+
+
+			//Saving and Converting 
+			if (MD != "") {
+				string Tex = convert(MD);
+				file_write(file_out, Tex);
+				cout << "Saved as " << file_out;
+			}
+			else {
+				cout << "Error while reading file. It may be empty or not exist at all";
+			}
 		}
 	}
 }
